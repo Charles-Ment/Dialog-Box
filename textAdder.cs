3337c75f582166f934bf8e4f56f6ac779872ac38
Charles-Ment <84221332+Charles-Ment@users.noreply.github.com>
2021-05-15 08:00:40 +0100

    Added Text Controller
    This script adds one character at a time at each interval so that it appears to being typed.

---
+++ textAdder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textAdder : MonoBehaviour
+{
+    float timeCount=0;
+    int count=0;
+    public float typingSpeed;
+    public Text text;
+    public TextAsset lines;
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        timeCount += Time.deltaTime;
+        if (timeCount > 1/(typingSpeed))
+        {
+            if (count < lines.text.Length)
+            {
+                timeCount = 0;
+                text.text += lines.text[count];
+                count++;
+
+            }
+
+        }
+
+    }
+}
