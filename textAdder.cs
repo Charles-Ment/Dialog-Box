03ee42fda73236ca0b5019272776e833f248762f
Charles-Ment <84221332+Charles-Ment@users.noreply.github.com>
2021-05-15 08:09:22 +0100

    Added Comments

--- textAdder.cs
+++ textAdder.cs
@@ -7,10 +7,11 @@ public class textAdder : MonoBehaviour
 {
     float timeCount=0;
     int count=0;
+    
     public float typingSpeed;
     public Text text;
     public TextAsset lines;
-    // Start is called before the first frame update
+    
     void Start()
     {
         text.text = "";
@@ -19,12 +20,16 @@ public class textAdder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // The Time.deltaTime (the time for each frames) is added to the timeCount variable to obtain the time since the last change to the text.
         timeCount += Time.deltaTime;
+        
+        // Everytime the time count is above a certain time then it appends the text to the public text object.
         if (timeCount > 1/(typingSpeed))
         {
+            // Checks if the count is still less than the length of the text inputted so that there isn't an OutOfBoundsError.
             if (count < lines.text.Length)
             {
+                // Resets the timeCount variable.
                 timeCount = 0;
                 text.text += lines.text[count];
                 count++;
